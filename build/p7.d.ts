649746d395425863b40665bc5aace9dbdd952b00
Łukasz Basaj <lbasaj@gmail.com>
2019-10-27 16:41:33 +0100

    Add class and interface

---
+++ build/p7.d.ts
@@ -0,0 +1,9 @@
+interface X {
+    plus5(x: number): number;
+}
+declare class Y implements X {
+    minus5(x: number): number;
+    plus5(x: number): number;
+}
+declare const o: Y;
+declare function ff(x: X): void;
